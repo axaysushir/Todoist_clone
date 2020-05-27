931578b8ab1afce2227365ce33629db9c7730a98
axaysushir <sinewave_11@yahoo.com>
2020-05-27 14:35:18 +0530

    add redirect

---
+++ build/precache-manifest.bc9fdf6b75cbbe8f165a11af289b56df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "364c7a89f691122a1d92e858b27cb66c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f62ace8e400b16c2e1f",
+    "url": "/static/css/main.385a2693.chunk.css"
+  },
+  {
+    "revision": "68fcbbf72e9d06105cfc",
+    "url": "/static/js/2.45a70091.chunk.js"
+  },
+  {
+    "revision": "3aa8d522e29fa9142f467aacdad35f45",
+    "url": "/static/js/2.45a70091.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f62ace8e400b16c2e1f",
+    "url": "/static/js/main.f43ae0c9.chunk.js"
+  },
+  {
+    "revision": "e3aad96969bd4469b73c",
+    "url": "/static/js/runtime-main.fa9fae9c.js"
+  },
+  {
+    "revision": "a6cbd140772bfd97417b79e8932e49b7",
+    "url": "/static/media/icon.a6cbd140.svg"
+  }
+]);
